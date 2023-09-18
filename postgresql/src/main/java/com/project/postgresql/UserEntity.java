95da2383b301144d0088428e0db6e4f15c59b55c
ibrahima diagne seck <ibrahimadiagneseck@esp.sn>
2023-09-18 14:29:58 +0000

    first commit

---
+++ postgresql/src/main/java/com/project/postgresql/UserEntity.java
@@ -0,0 +1,54 @@
+package com.project.postgresql;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
